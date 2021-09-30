d966a5be4cbd2428f6cf26fa63bd67e7ed474cf7
bohucky <you@example.com>
2021-09-30 10:52:12 +0200

    nepodstatne

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
 <title>Page Title</title>
 </head>
 <body>
 
-<h1>My First Heading</h1>
+<h1>My First Hing</h1>
 <p>My first paragraph.</p>
 
 </body>
